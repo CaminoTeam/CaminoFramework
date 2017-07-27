804e18da59beab33c03712c3cf3cc50523c562d9
CaminoTeam <abc@def.ghi>
2017-07-28 00:27:42 +0800

    squash pushed camino framework

---
+++ CaminoLib/CaminoLib/ClPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaminoLib
+{
+    // place scores and player unique stuff in here
+    public class ClPlayer
+    {
+        public string Nickname { get; set; }
+        public int Score { get; set; }
+        public ClPlayer()
+        {
+            Nickname = "Unnamed Player";
+            Score = 0;
+        }
+
+        public ClPlayer(string nickname) : this()
+        {
+            this.Nickname = nickname;
+        }
+    }
+}
